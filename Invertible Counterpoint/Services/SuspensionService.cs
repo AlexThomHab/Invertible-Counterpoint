a3541f363cc7ecdbfd724b06d7bcf8683b92213c
AlexThomHab <alex.haberfield@gmail.com>
2025-09-12 15:16:45 +0100

    clean up
    clean up

--- Invertible Counterpoint/Services/SuspensionService.cs
+++ Invertible Counterpoint/Services/SuspensionService.cs
@@ -32,47 +32,7 @@ namespace Invertible_Counterpoint.Services
             return jv0Interval;
         }
 
-        private static int Rank(SuspensionTreatmentEnum x) => x switch
-        {
-            SuspensionTreatmentEnum.CannotFormSuspension => 0, // strictest
-            SuspensionTreatmentEnum.IfOnDownbeatMustFormSuspensionAndNoteOfResolutionIsDissonant => 1,
-            SuspensionTreatmentEnum.IfOnDownbeatMustFormSuspension => 2,
-            SuspensionTreatmentEnum.NoteOfResolutionIsDissonant => 3,
-            SuspensionTreatmentEnum.NoteOfResolutionIsFree => 4, // softest
-            _ => 4
-        };
-
-        public static SuspensionTreatmentEnum StrictMost(
-            SuspensionTreatmentEnum a,
-            SuspensionTreatmentEnum b)
-        {
-            if (a == b) return a;
-
-            if (a == SuspensionTreatmentEnum.CannotFormSuspension ||
-                b == SuspensionTreatmentEnum.CannotFormSuspension)
-                return SuspensionTreatmentEnum.CannotFormSuspension;
-
-            if (a == SuspensionTreatmentEnum.IfOnDownbeatMustFormSuspensionAndNoteOfResolutionIsDissonant ||
-                b == SuspensionTreatmentEnum.IfOnDownbeatMustFormSuspensionAndNoteOfResolutionIsDissonant)
-                return SuspensionTreatmentEnum.IfOnDownbeatMustFormSuspensionAndNoteOfResolutionIsDissonant;
-
-            bool hasMust = a == SuspensionTreatmentEnum.IfOnDownbeatMustFormSuspension
-                           || b == SuspensionTreatmentEnum.IfOnDownbeatMustFormSuspension;
-            bool hasX = a == SuspensionTreatmentEnum.NoteOfResolutionIsDissonant
-                        || b == SuspensionTreatmentEnum.NoteOfResolutionIsDissonant;
-
-            if (hasMust && hasX)
-                return SuspensionTreatmentEnum.IfOnDownbeatMustFormSuspensionAndNoteOfResolutionIsDissonant;
-
-            return (Rank(a) <= Rank(b)) ? a : b;
-        }
-
-        public static SuspensionTreatmentEnum StrictMost(params SuspensionTreatmentEnum[] values) =>
-            values.Aggregate(SuspensionTreatmentEnum.NoteOfResolutionIsFree, StrictMost);
-
-        private static Interval CopyInterval(Interval sourceInterval) =>
-            new Interval(sourceInterval.Number, sourceInterval.Name, sourceInterval.IsConsonant,
-                sourceInterval.UpperSuspensionTreatmentEnum, sourceInterval.LowerSuspensionTreatmentEnum);
+       
         private static SuspensionTreatmentEnum StrictMostSuspensionTreatmentEnum(
             SuspensionTreatmentEnum originalIntervalSuspension,
             SuspensionTreatmentEnum newIntervalSuspension)
