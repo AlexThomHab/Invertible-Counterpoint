0ef6eed0346adca977f17f21140d05933aba5e08
AlexThomHab <alex.haberfield@gmail.com>
2025-09-12 15:15:05 +0100

    removed commets
    removed commets

--- Tests/TwoVoiceVerticalShiftCouterpoint/IntervalInversionCalculation/GivenANegativeJvIndex.cs
+++ Tests/TwoVoiceVerticalShiftCouterpoint/IntervalInversionCalculation/GivenANegativeJvIndex.cs
@@ -14,14 +14,14 @@ namespace Tests.TwoVoiceVerticalShiftCouterpoint.IntervalInversionCalculation
             _calculator = new TwoVoiceShiftedIntervalsGivenJvIndexCalculator();
         }
 
-        [TestCase(-11, new[] { 0, 2, 4, 7 }, new[] { 1, 3 }, new[] { 5 }, new[] { 6 }, new int[0])] // -11 ≡ 3 → none
-        [TestCase(-7, new[] { 0, 2, 5, 7 }, new[] { 1, 6 }, new[] { 4 }, new[] { 3 }, new int[0])] // -7  ≡ 0 → none
-        [TestCase(-6, new[] { 2, 4 }, new[] { 3 }, new[] { 0, 5, 7 }, new[] { 1, 6 }, new[] { 2 })] // -6 ≡ 1 → none
-        [TestCase(-5, new[] { 0, 5, 7 }, new[] { 6 }, new[] { 2, 4 }, new[] { 1, 3 }, new[] { 5 })] // -5 ≡ 2 → {2,5}∩fixed = {5}
-        [TestCase(-4, new[] { 0, 2, 4 }, new[] { 1, 3 }, new[] { 5, 7 }, new[] { 6 }, new int[0])] // -4 ≡ 3 → none
-        [TestCase(-3, new[] { 5, 7 }, new[] { 6 }, new[] { 0, 2, 4 }, new[] { 1, 3 }, new int[0])] // -3 ≡ 4 → none
-        [TestCase(-2, new[] { 0, 2, 4, 7 }, new[] { 1, 3 }, new[] { 5 }, new[] { 6 }, new[] { 2 })] // -2 ≡ 5 → {2}
-        [TestCase(-1, new[] { 5 }, new int[0], new[] { 0, 2, 4, 7 }, new[] { 1, 3, 6 }, new[] { 5 })] // -1 ≡ 6 → {5}
+        [TestCase(-11, new[] { 0, 2, 4, 7 }, new[] { 1, 3 }, new[] { 5 }, new[] { 6 }, new int[0])]
+        [TestCase(-7, new[] { 0, 2, 5, 7 }, new[] { 1, 6 }, new[] { 4 }, new[] { 3 }, new int[0])]
+        [TestCase(-6, new[] { 2, 4 }, new[] { 3 }, new[] { 0, 5, 7 }, new[] { 1, 6 }, new[] { 2 })]
+        [TestCase(-5, new[] { 0, 5, 7 }, new[] { 6 }, new[] { 2, 4 }, new[] { 1, 3 }, new[] { 5 })]
+        [TestCase(-4, new[] { 0, 2, 4 }, new[] { 1, 3 }, new[] { 5, 7 }, new[] { 6 }, new int[0])]
+        [TestCase(-3, new[] { 5, 7 }, new[] { 6 }, new[] { 0, 2, 4 }, new[] { 1, 3 }, new int[0])]
+        [TestCase(-2, new[] { 0, 2, 4, 7 }, new[] { 1, 3 }, new[] { 5 }, new[] { 6 }, new[] { 2 })]
+        [TestCase(-1, new[] { 5 }, new int[0], new[] { 0, 2, 4, 7 }, new[] { 1, 3, 6 }, new[] { 5 })]
         public void NegativeJV_CasesPass(
             int jvIndex,
             int[] expectedFixedCons,
